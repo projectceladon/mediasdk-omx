373ee17f9bcf913d290d4dfb2f6248285d28f1ef
zhezhu <zhe.zhu@intel.com>
2019-09-17 11:32:44 +0800

    Add timestamp msg in case no output buffer available
    Tracked-On: ACI-5980
    
    Signed-off-by: Li, Yingjie <yingjie.li@intel.com>

--- omx_components/src/mfx_omx_vdec_component.cpp
+++ omx_components/src/mfx_omx_vdec_component.cpp
@@ -618,10 +618,10 @@ OMX_ERRORTYPE MfxOmxVdecComponent::DealWithBuffer(
             }
         }
         else 
-	{
-	    MFX_OMX_LOG_ERROR("Failed to allocate buffers, pBufferHeader=(%p), pBuffer=(%p)", pBufferHeader, pBuffer);
-	    mfx_sts = MFX_ERR_MEMORY_ALLOC;
-	}
+        {
+            MFX_OMX_LOG_ERROR("Failed to allocate buffers for decode output, pBufferHeader=(%p), pBuffer=(%p), nTimeStamp %lld", pBufferHeader, pBuffer, (*ppBufferHdr)->nTimeStamp);
+            mfx_sts = MFX_ERR_MEMORY_ALLOC;
+        }
 
         if (MFX_ERR_NONE == mfx_sts)
         {
